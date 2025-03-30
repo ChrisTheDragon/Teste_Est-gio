b19bfc1462d185fb6dc68bda6d0f599a8bb7737e
Christian Marinho <88001806+ChrisTheDragon@users.noreply.github.com>
2025-03-30 19:45:44 -0300

    refatora app.py para melhorar a leitura do código e remove script main_teste_depreciat.py obsoleto

--- Teste de API/backend/app.py
+++ Teste de API/backend/app.py
@@ -1,29 +1,27 @@
 from flask import Flask, request, jsonify
 import pandas as pd
-from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)  # Permite requisições do frontend
 
 # Carrega os dados do CSV
-df = pd.read_csv(r"Teste de API\backend\Relatorio_cadop.csv", sep=';', encoding='utf-8')
-df = df.fillna('')  # Substitui todos os valores NaN por uma string vazia
+df = pd.read_csv(
+    r"Teste de API\backend\Relatorio_cadop.csv", 
+    sep=';', 
+    encoding='utf-8'
+)
+df = df.fillna('')  # Substitui valores NaN por strings vazias
 
 @app.route('/api/search', methods=['GET'])
 def search_operadoras():
+    # Obtém o termo de busca da query string
     termo = request.args.get('q', '').lower()
-    print(f"Termo de busca: {termo}")  # Log do termo de busca
+    print(f"Termo de busca: {termo}")
     
+    # Retorna uma lista vazia se nenhum termo for fornecido
     if not termo:
         return jsonify([])
     
-    # Garantir que as colunas sejam strings e preencher valores nulos
-    df['Registro_ANS'] = df['Registro_ANS'].fillna("").astype(str)
-    df['CNPJ'] = df['CNPJ'].fillna("").astype(str)
-    df['Nome_Fantasia'] = df['Nome_Fantasia'].fillna("").astype(str)
-    df['Razao_Social'] = df['Razao_Social'].fillna("").astype(str)
-    
-    # Filtra operadoras que contenham o termo no nome ou código
+    # Filtra os dados com base no termo de busca
     resultados = df[
         df['Registro_ANS'].str.lower().str.contains(termo) |
         df['CNPJ'].str.contains(termo) |
@@ -31,8 +29,10 @@ def search_operadoras():
         df['Razao_Social'].str.lower().str.contains(termo)
     ].head(20).to_dict('records')
     
-    print(f"Resultados encontrados: {len(resultados)}")  # Log do número de resultados
+    # Log do número de resultados encontrados
+    print(f"Resultados encontrados: {len(resultados)}")
     
+    # Retorna os resultados como JSON
     return jsonify(resultados)
 
 if __name__ == '__main__':
